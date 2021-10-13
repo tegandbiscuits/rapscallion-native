8a1c08e88672f35547c5cc01a13ef92fb1cc424b
Tegan Rauh <onlysayinghi@gmail.com>
2021-10-12 19:59:43 -0500

    Build out a bit of the UI

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import Game from './src/App';
+
+const theme: typeof DefaultTheme = {
+  ...DefaultTheme,
+  mode: 'adaptive',
+  dark: true,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#fff',
+    background: '#152624',
+    text: '#777',
+  },
+};
 
 export default function App() {
   return (
-    <Game />
+    <PaperProvider theme={theme}>
+      <Game />
+    </PaperProvider>
   );
 }
