eb0afdbc724358fd0f038ccdd3a2db56d2fac5b7
Tegan Rauh <onlysayinghi@gmail.com>
2021-10-29 17:22:54 -0500

    Styles for cards

--- src/decks/PlayCard.tsx
+++ src/decks/PlayCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Surface, Text, TouchableRipple } from 'react-native-paper';
+import { StyleProp, StyleSheet, TextStyle, View } from 'react-native';
+import { Surface, Text, TouchableRipple, Colors } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Potion from '../icons/Potion';
 import Shield from '../icons/Shield';
 
@@ -15,23 +16,84 @@ interface Props extends IPlayCard {
   onPress: (hpChange: number) => void;
 }
 
+const cardImageSize = 120;
 const styles = StyleSheet.create({
   card: {
     width: 150,
     height: 200,
     margin: 5,
+    position: 'relative',
+  },
+  cardInfo: {
+    position: 'absolute',
+    alignItems: 'center',
+    top: '5%',
+    left: '5%',
+  },
+  cardImage: {
+    position: 'absolute',
+    height: cardImageSize,
+    width: cardImageSize,
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: -(cardImageSize / 2) }, { translateY: -(cardImageSize / 2) }],
+    zIndex: -1,
+  },
+  cardLabel: {
+    position: 'absolute',
+    bottom: '5%',
+    width: '100%',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  points: {
+    fontSize: 21,
+  },
+  cardSuit: {
+    fontSize: 20,
   },
 });
 
-const CenterImage = (cardType: CardTypes) => {
+const CenterImage = ({ cardType, color }: { cardType: CardTypes, color: string }) => {
   switch (cardType) {
     case 'potion':
-      return <Potion />;
+      return <Potion style={styles.cardImage} fill={color} />;
     case 'shield':
-      return <Shield />;
+      return <Shield style={styles.cardImage} fill={color} />;
     default:
-      return <Text>(dragon was jank)</Text>;
+      return <Text style={[styles.cardImage, { color }]}>(dragon was jank)</Text>;
   }
+};
+
+const suitIcons: Record<IPlayCard['suit'], string | null> = {
+  clubs: 'cards-club',
+  diamonds: 'cards-diamond',
+  hearts: 'cards-heart',
+  jack: null,
+  joker: null,
+  spades: 'cards-spade',
+};
+
+const SuitImage = ({
+  suit,
+  style,
+}: {
+  suit: IPlayCard['suit'],
+  style: StyleProp<TextStyle>,
+}) => {
+  const suitName = suitIcons[suit];
+
+  if (!suitIcons) {
+    return null;
+  }
+
+  return (
+    <MaterialCommunityIcons
+      style={style}
+      // @ts-expect-error unable to ensure name is set more specific than string
+      name={suitName}
+    />
+  );
 };
 
 const PlayCard = ({ onPress, suit, rank }: Props) => {
@@ -54,6 +116,8 @@ const PlayCard = ({ onPress, suit, rank }: Props) => {
     pointModification = -pointModification;
   }
 
+  const color = cardType === 'enemy' ? Colors.black : Colors.red500;
+
   const handlePress = () => {
     const hpChange = cardType !== 'shield' ? pointModification : 0;
     onPress(hpChange);
@@ -66,12 +130,14 @@ const PlayCard = ({ onPress, suit, rank }: Props) => {
       accessibilityLabel={`${cardLabel} card, ${pointModification} points`} // TODO: i18n to have the 's' or not
     >
       <Surface style={styles.card}>
-        <View>
-          <Text>{pointModification}</Text>
-          <Text>{suit}</Text>
+        <View style={styles.cardInfo}>
+          <Text style={[styles.points, { color }]}>{pointModification}</Text>
+          <SuitImage style={[styles.cardSuit, { color }]} suit={suit} />
         </View>
-        {CenterImage(cardType)}
-        <Text>{cardLabel}</Text>
+
+        <CenterImage cardType={cardType} color={color} />
+
+        <Text style={[styles.cardLabel, { color }]}>{cardLabel}</Text>
       </Surface>
     </TouchableRipple>
   );
