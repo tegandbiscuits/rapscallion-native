e89d07897faf1e6b9b0301029a93d75c7f3ddfe4
Tegan Rauh <onlysayinghi@gmail.com>
2022-01-20 21:36:44 -0600

    Use react navigation

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   StyleSheet,
   View,
 } from 'react-native';
-import { Headline, useTheme } from 'react-native-paper';
+import { Headline, useTheme, Button } from 'react-native-paper';
 import { useDispatch } from 'react-redux';
-import MainMenu from './MainMenu';
-import Game, { GameModes } from './Game';
+import { useNavigation } from '@react-navigation/native';
+import Knot from './icons/Knot';
 import { shuffleDeck } from './state/gameSlice';
 import Decks from './decks/decks';
 
@@ -16,6 +16,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
+  mainContent: {
+    alignItems: 'center',
+  },
   title: {
     marginBottom: 30,
   },
@@ -23,13 +26,15 @@ const styles = StyleSheet.create({
 
 const App = () => {
   const theme = useTheme();
-  const [gameMode, setGameMode] = useState<GameModes | null>(null);
-  const inGame = !!gameMode;
+  // const [gameMode, setGameMode] = useState<GameModes | null>(null);
+  // const inGame = !!gameMode;
   const dispatch = useDispatch();
+  const navigation = useNavigation<any>();
 
   const startGame = () => {
     dispatch(shuffleDeck(Decks.Standard));
-    setGameMode(GameModes.Standard);
+    navigation.navigate('Game');
+    // setGameMode(GameModes.Standard);
   };
 
   return (
@@ -38,11 +43,12 @@ const App = () => {
         rapscallion
       </Headline>
 
-      {!inGame && (
-        <MainMenu onGameStart={startGame} />
-      )}
-
-      {(inGame && gameMode != null) && <Game />}
+      <View style={styles.mainContent}>
+        <Knot width={150} height={150} />
+        <Button mode="outlined" onPress={startGame}>
+          Standard
+        </Button>
+      </View>
     </View>
   );
 };
